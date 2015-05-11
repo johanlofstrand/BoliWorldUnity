97916ef6a779ed8f29074d26676ffb94a5ae0ead
Johan Löfstrand <johan.lofstrand@svt.se>
2015-05-11 15:51:17 +0200

    first balloons on screen

---
+++ Assets/Scripts/BalloonStartController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonStartController : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject sprite;
+
+	// Use this for initialization
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main; 		
+		}
+//		GameObject gameControllerObject = GameObject.FindWithTag ("score");
+//		Debug.Log (gameControllerObject);
+	}
+
+	void OnLeftClick(){
+		Debug.Log ("Left click!");
+		createNewBalloons ();
+	}
+
+	void createNewBalloons() {
+		GameObject balloonObject = GameObject.Instantiate(Resources.Load("blue")) as GameObject;
+		balloonObject.transform.position = new Vector3(Random.Range (-5,5),0,0);
+	}
+
+	void OnRightClick(){
+		Debug.Log ("Right click!");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
