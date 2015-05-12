444bc43b8991a36f80a3a705ea6dc949b7451ebb
Johan Löfstrand <johan.lofstrand@svt.se>
2015-05-12 16:42:44 +0200

    first working balloon version

--- Assets/Scripts/BalloonStartController.cs
+++ Assets/Scripts/BalloonStartController.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class BalloonStartController : MonoBehaviour {
 
 	public Camera cam;
-	public GameObject sprite;
+	public GameObject balloon;
+	public GUIText Score;
+	public int scoreCounter;
+	private float maxWidth;
+	private float balloonWidth;
 
 	// Use this for initialization
 	void Start () {
 		if (cam == null) {
 			cam = Camera.main; 		
 		}
-//		GameObject gameControllerObject = GameObject.FindWithTag ("score");
-//		Debug.Log (gameControllerObject);
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		balloonWidth = balloon.GetComponent<Renderer>().bounds.extents.x;
+		maxWidth = targetWidth.x - balloonWidth;
+		scoreCounter = 0;
 	}
 
 	void OnLeftClick(){
@@ -21,15 +28,22 @@ public class BalloonStartController : MonoBehaviour {
 	}
 
 	void createNewBalloons() {
-		GameObject balloonObject = GameObject.Instantiate(Resources.Load("blue")) as GameObject;
-		balloonObject.transform.position = new Vector3(Random.Range (-5,5),0,0);
+		Instantiate(balloon);
+		Vector3 pos = balloon.transform.position;
+		pos.x = Random.Range (-maxWidth,maxWidth);
+		balloon.transform.position = pos;
 	}
 
 	void OnRightClick(){
 		Debug.Log ("Right click!");
 	}
 
+	public void updateScore() {
+		scoreCounter++;
+	}
+
 	// Update is called once per frame
 	void Update () {
+		Score.text = "Score: " + scoreCounter;
 	}
 }
