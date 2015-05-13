8115f754c6bb8f1fd08a87b87330e8954b4f9676
Johan Löfstrand <johan.lofstrand@svt.se>
2015-05-13 14:09:19 +0200

    final demo version

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonController : MonoBehaviour {
+
+	Sprite pop;
+	float vx = 0.01f;
+	float direction = 1f;
+	BalloonStartController gameController;
+
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <BalloonStartController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+		pop = Resources.Load <Sprite> ("blue_pop");
+		direction = rightOrLeft();
+	}
+
+	void OnLeftClick(){
+		gameController.updateScore ();
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>(); 
+		spriteRenderer.sprite = pop; 
+		AudioSource sound = gameObject.GetComponent<AudioSource>();
+		sound.Play();
+		StartCoroutine (WaitPop());
+	}
+
+	IEnumerator WaitPop()
+	{
+		yield return new WaitForSeconds(0.6f);
+		Destroy(this.gameObject);
+	}
+	
+	void Update () {
+		var pos = this.gameObject.transform.position;
+		pos.x += vx * direction;
+		this.gameObject.transform.position = pos;
+	}
+
+	int rightOrLeft() {
+		return Random.value > 0.5 ? -1 : 1;
+	}
+
+
+	/*
+	  public class BalloonController (){
+     static public GameObject MakeObject(String balloonColor){
+         GameObject clone = (GameObject)Instantiate(playerPrefab);
+         clone.GetComponent<Controller>().balloonColor = balloonColor;
+         return clone;
+     }
+ }*/
+
+}
---
+++ Assets/Scripts/OutOfBoundsDestroyer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfBoundsDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D obj) {
+		Debug.Log ("Destroyed balloon that's out of bounds: " + obj.gameObject.name);
+		Destroy (obj.gameObject);
+	}
+}
